286241c329e1d3a0c804d3cd3097bfa1c9d79b31
Tatiana251 <tata.588@hotmail.com>
2022-04-25 23:59:50 -0500

    Crear tabla cuenta
    Se creo tabla cuenta, y se realizo la migración

---
+++ database/migrations/2022_04_26_043035_create_cuenta_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuenta', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 20)->unique();
+            $table->string('descripcion');
+            $table->double('valor');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cuenta', function(Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        } );
+        Schema::dropIfExists('cuenta');
+    }
+
+};
